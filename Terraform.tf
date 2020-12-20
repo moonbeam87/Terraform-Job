58c34cfe6d7a2c32c78692a57346bd3e3d71ae8f
Dev <2021dpatel@gmail.com>
2020-12-20 10:12:19 -0500

    launch script added

--- Terraform.tf
+++ Terraform.tf
@@ -6,3 +6,31 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "EC2-Instance"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "test-instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Test Instance"
+  }
+}
