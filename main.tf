b5171c94424e334483c2e9655ca4a42d3c933e6e
Dev <2021dpatel@gmail.com>
2020-12-21 10:14:58 -0500

    handler updated

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename = "LambdaExample.zip"
   function_name = "lambda_function_name"
   role = aws_iam_role.iam_for_lambda.arn
-
+  handler = "lambda_function.lambda_handler"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
