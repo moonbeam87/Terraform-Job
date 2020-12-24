47d98d8707e2f953245750b6de55d796e3abe602
Dev <2021dpatel@gmail.com>
2020-12-24 09:36:39 -0500

    ami id changed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #EC2 Terraform
 resource "aws_instance" "TestInstance" {
-  ami = "ami-0b69ea66ff7391e80"
+  ami = "	ami-053adf54573f777cf"
   instance_type = "t2.micro"
   tags = {
     Name = "EC2 Terraform Job"
